50f18f781bbf507fbfa6a56f65f28c6a3b19799b
scottbedard <scottbedard90@gmail.com>
2015-04-24 08:46:50 -0400

    Adds component property for direction buttons

--- components/Slider.php
+++ components/Slider.php
@@ -24,6 +24,24 @@ class Slider extends ComponentBase
         ];
     }
 
+    public function defineProperties()
+    {
+        return [
+            'prevArrow' => [
+                 'title'             => 'Previous arrow',
+                 'description'       => 'Markup for the previous slide button.',
+                 'type'              => 'string',
+                 'showExternalParam' => false,
+            ],
+            'nextArrow' => [
+                 'title'             => 'Next arrow',
+                 'description'       => 'Markup for the next slide button.',
+                 'type'              => 'string',
+                 'showExternalParam' => false,
+            ],
+        ];
+    }
+
     public function onRun()
     {
         // Query our active slides
@@ -42,7 +60,9 @@ class Slider extends ComponentBase
             'autoplaySpeed'     => intval(Settings::get('autoplaySpeed', 3000)),
             'cssEase'           => Settings::get('cssEase', 'ease'),
             'dots'              => (bool) Settings::get('dots', false),
+            'nextArrow'         => $this->property('nextArrow') ?: '<button type="button" class="slick-next">Next</button>',
             'pauseOnHover'      => (bool) Settings::get('pauseOnHover', true),
+            'prevArrow'         => $this->property('prevArrow') ?: '<button type="button" class="slick-prev">Prev</button>',
             'touchThreshold'    => intval(Settings::get('touchThreshold', 5)),
         ];
 
